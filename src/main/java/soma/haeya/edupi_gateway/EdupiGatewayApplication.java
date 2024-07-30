e2a8fdf1a39811fc39f9f017b8a729c878e57d33
GS_song98 <20003204@sju.ac.kr>
2024-07-30 18:26:43 +0900

    [#2]chore: 프로젝트 기본 환경 설정

---
+++ src/main/java/soma/haeya/edupi_gateway/EdupiGatewayApplication.java
@@ -0,0 +1,13 @@
+package soma.haeya.edupi_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soma/haeya/edupi_gateway/EdupiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package soma.haeya.edupi_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdupiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
