36390f4bee784fce9c1c51b7743cff30c268ea20
GS_song98 <20003204@sju.ac.kr>
2024-08-08 13:08:14 +0900

    [#3]style: config 하드코딩을 yml파일로 이동

--- src/main/java/soma/haeya/edupi_gateway/config/RoutingConfig.java
+++ src/main/java/soma/haeya/edupi_gateway/config/RoutingConfig.java
@@ -1,5 +1,6 @@
 package soma.haeya.edupi_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,20 +10,36 @@ import soma.haeya.edupi_gateway.filter.AuthenticationFilter;
 @Configuration
 public class RoutingConfig {
 
+    @Value("${server-url.db.url}")
+    private String DB_URL;
+    @Value("${server-url.db.default-path}")
+    private String DB_PATH;
+
+    @Value("${server-url.user.url}")
+    private String USER_URL;
+    @Value("${server-url.user.default-path}")
+    private String USER_PATH;
+
+    @Value("${server-url.visualize.url}")
+    private String VISUALIZE_URL;
+    @Value("${server-url.visualize.default-path}")
+    private String VISUALIZE_PATH;
+
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder, AuthenticationFilter authorizationFilter) {
         return builder.routes()
 
-            .route(predicate -> predicate.path("/edupi_visualize/**")
+            .route(predicate -> predicate.path(VISUALIZE_PATH)
                 .filters(f -> f.filter(authorizationFilter.apply(config -> config.setRequiredRole("ROLE_USER"))))
-                .uri("http://localhost:8081/")) // code_visualize
+                .uri(VISUALIZE_URL)) // code_visualize
 
-            .route(predicate -> predicate.path("/edupi_user/**")
-                .uri("http://localhost:8083/")) // edupi_user
+            .route(predicate -> predicate.path(USER_PATH)
+                .uri(USER_URL)) // edupi_user
 
-            .route(predicate -> predicate.path("/edupi_db/**")
-                .filters(f -> f.filter(authorizationFilter.apply(config -> config.setRequiredRole("ROLE_USER"))))
-                .uri("http://localhost:8084/")) // edupi_db
+            .route(predicate -> predicate.path(DB_PATH)
+                .uri(DB_URL)) // edupi_db
+
             .build();
     }
 
