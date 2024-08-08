5b1d465f2b12d11824ebd177f1669b4bf6c28130
GS_song98 <20003204@sju.ac.kr>
2024-08-08 13:10:47 +0900

    [#3]remove: 중복코드 제거

--- src/main/java/soma/haeya/edupi_gateway/exception/GlobalExceptionHandler.java
+++ src/main/java/soma/haeya/edupi_gateway/exception/GlobalExceptionHandler.java
@@ -4,15 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import soma.haeya.edupi_gateway.dto.ErrorResponse;
+import soma.haeya.edupi_gateway.models.ErrorResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UnAuthorizedException.class)
     public ResponseEntity<ErrorResponse> handleValidationExceptions(UnAuthorizedException ex) {
 
