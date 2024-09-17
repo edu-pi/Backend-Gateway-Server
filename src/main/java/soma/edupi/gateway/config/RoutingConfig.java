1136bdf4feacce92758fa3e23bd5674cbac47a88
GS_song98 <20003204@sju.ac.kr>
2024-09-17 21:52:36 +0900

    [#19]rename: X-User-Id -> X-Account-Id

--- src/main/java/soma/edupi/gateway/config/RoutingConfig.java
+++ src/main/java/soma/edupi/gateway/config/RoutingConfig.java
@@ -20,6 +20,11 @@ public class RoutingConfig {
     @Value("${server-url.user.default-path}")
     private String USER_PATH;
 
+//    @Value("${server-url.lms.url}")
+//    private String LMS_URL;
+//    @Value("${server-url.lms.default-path}")
+//    private String LMS_PATH;
+
     @Value("${server-url.visualize.url}")
     private String VISUALIZE_URL;
     @Value("${server-url.visualize.default-path}")
@@ -32,15 +37,20 @@ public class RoutingConfig {
             .route("root_route", r -> r.path("/")
                 .filters(f -> f.setStatus(200))
                 .uri("no://op")) // health check
+
             .route(predicate -> predicate.path(VISUALIZE_PATH)
 //                .filters(f -> f.filter(authorizationFilter.apply(config -> config.setRequiredRole("ROLE_USER"))))
-                .uri(VISUALIZE_URL)) // code_visualize
+                .uri(VISUALIZE_URL)) // edupi-visualize
+
+//            .route(predicate -> predicate.path(LMS_PATH)
+//                .filters(f -> f.filter(authorizationFilter.apply(config -> config.setRequiredRole("ROLE_USER"))))
+//                .uri(LMS_URL)) // edupi-lms
 
             .route(predicate -> predicate.path(USER_PATH)
-                .uri(USER_URL)) // edupi_user
+                .uri(USER_URL)) // edupi-user
 
             .route(predicate -> predicate.path(DB_PATH)
-                .uri(DB_URL)) // edupi_db
+                .uri(DB_URL)) // edupi-db
 
             .build();
     }
